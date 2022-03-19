6bcee2d29a38c1e777a96776f067143f08352d92
Greg Peddle <gpeddle@gmail.com>
2022-03-18 22:28:59 -0400

    added 0002

---
+++ 0002.add-two-numbers/0002.add-two-numbers.go
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return &ListNode{Val: 1}
+}
---
+++ 0002.add-two-numbers/0002.add-two-numbers_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+
+	t.Parallel()
+	
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
