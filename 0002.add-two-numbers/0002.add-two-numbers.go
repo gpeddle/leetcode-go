8cdc8213f686c963213fc7cc82bd38ec067ae604
Greg Peddle <gpeddle@gmail.com>
2022-03-19 22:35:04 -0400

    0002 solution;fails on large list

--- 0002.add-two-numbers/0002.add-two-numbers.go
+++ 0002.add-two-numbers/0002.add-two-numbers.go
@@ -1,5 +1,9 @@
 package leetcode
 
+import (
+	"math"
+)
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -14,5 +18,79 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return &ListNode{Val: 1}
+
+	n1 := toInteger(l1)
+	if n1 == 0 {
+		return l2
+	}
+	n2 := toInteger(l2)
+	if n2 == 0 {
+		return l1
+	}
+
+	sum := n1 + n2
+
+	result := toList(sum)
+	return result
 }
+
+func toInteger(list *ListNode) int {
+
+	var result = 0
+	pow := 0
+	for list != nil {
+		result += list.Val * int(math.Pow10(pow))
+		pow++
+		list = list.Next
+	}
+	return result
+}
+
+func toList(num int) *ListNode {
+
+	var (
+		head     *ListNode
+		current  *ListNode
+		previous *ListNode
+	)
+	working := num
+
+	for working != 0 {
+
+		digit := working % 10
+		working = (working - digit) / 10
+		current = &ListNode{
+			Val:  digit,
+			Next: nil,
+		}
+
+		if head == nil {
+			head = current
+		} else {
+			previous.Next = current
+		}
+		previous = current
+
+	}
+	return head
+}
+
+/*
+func reverse(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+	working := head.Next
+	reversed := head
+	reversed.Next = nil
+
+	for working != nil {
+		tmp := working
+		working = working.Next
+		tmp.Next = reversed
+		reversed = tmp
+	}
+	return reversed
+}
+*/
--- 0002.add-two-numbers/0002.add-two-numbers_test.go
+++ 0002.add-two-numbers/0002.add-two-numbers_test.go
@@ -8,18 +8,68 @@ import (
 func Test_addTwoNumbers(t *testing.T) {
 
 	t.Parallel()
-	
+
 	type args struct {
 		l1 *ListNode
 		l2 *ListNode
 	}
+
 	tests := []struct {
 		name string
 		args args
 		want *ListNode
 	}{
-		// TODO: Add test cases.
+		{
+			name: "example 1",
+			args: args{
+				l1: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: nil}}},
+				l2: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: nil}}},
+			},
+			want: &ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: nil}}},
+		},
+		{
+			name: "example 2",
+			args: args{
+				l1: &ListNode{Val: 0, Next: nil},
+				l2: &ListNode{Val: 0, Next: nil},
+			},
+			want: &ListNode{Val: 0, Next: nil},
+		},
+		{
+			name: "example 3",
+			args: args{
+				l1: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: nil}}}}}}},
+				l2: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: nil}}}},
+			},
+			want: &ListNode{Val: 8, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: nil}}}}}}}},
+		},
+
+		{
+			name: "example 4", // from judge
+			args: args{
+				l1: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 9, Next: nil}}},
+				l2: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: &ListNode{Val: 9, Next: nil}}}},
+			},
+			want: &ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 4, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: nil}}}}},
+		},
+		{
+			name: "example 5", // from judge
+			args: args{
+				l1: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: &ListNode{Val: 6, Next: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 8, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5, Next: &ListNode{Val: 7, Next: nil}}}}}}}}},
+				l2: &ListNode{Val: 6, Next: &ListNode{Val: 7, Next: &ListNode{Val: 8, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: &ListNode{Val: 5, Next: &ListNode{Val: 8, Next: &ListNode{Val: 9, Next: &ListNode{Val: 7, Next: nil}}}}}}}}},
+			},
+			want: &ListNode{Val: 6, Next: &ListNode{Val: 5, Next: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: &ListNode{Val: 4, Next: &ListNode{Val: 2, Next: &ListNode{Val: 5, Next: &ListNode{Val: 5, Next: &ListNode{Val: 1, Next: nil}}}}}}}}}},
+		},
+		{
+			name: "example 6", // from judge
+			args: args{
+				l1: &ListNode{Val: 1, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}},
+				l2: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: nil}}},
+			},
+			want: &ListNode{Val: 1, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: nil}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}},
+		},
 	}
+
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
