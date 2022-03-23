9d436888a06c97792166df8057c094a0f79c1038
Greg Peddle <gpeddle@gmail.com>
2022-03-23 09:22:13 -0400

    0011 added

---
+++ algorithms/0011-container-with-most-water/0011-container-with-most-water.go
@@ -0,0 +1,5 @@
+package leetcode
+
+func maxArea(height []int) int {
+	return 0
+}
---
+++ algorithms/0011-container-with-most-water/0011-container-with-most-water_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import "testing"
+
+func Test_maxArea(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "example-1",
+			args: args{
+				height: []int{1, 8, 6, 2, 5, 4, 8, 3, 7},
+			},
+			want: 49,
+		},
+		{
+			name: "example-2",
+			args: args{
+				height: []int{1, 1},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := maxArea(tt.args.height); got != tt.want {
+				t.Errorf("maxArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
